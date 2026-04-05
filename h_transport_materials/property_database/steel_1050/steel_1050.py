809394a30515ef90a43b84c08bec5e722d3a9739
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2026-04-05 09:49:54 -0400

    original reference for carbon steel props, .tar files, and diffs included

--- h_transport_materials/property_database/steel_1050/steel_1050.py
+++ h_transport_materials/property_database/steel_1050/steel_1050.py
@@ -1,47 +1,53 @@
 import h_transport_materials as htm
-from h_transport_materials import Permeability, Solubility
+from h_transport_materials import Permeability, Solubility, Diffusivity
 import numpy as np
 
 u = htm.ureg
 
-bibsource = """@techreport{san_marchi_technical_2012,
-    title = {Technical Reference for Hydrogen Compatibility of Materials},
-    author = {San Marchi, Christopher W. and Somerday, Brian P.},
-    number = {SAND2012-7321},
-    doi = {10.2172/1055634},
-    institution = {Sandia National Laboratories},
-    year = {2012},
-    month = {sep},
-    pages = {1100--23},
-}"""
-
 perm_data = np.genfromtxt(
-    htm.absolute_path("san_marchi_2012_permeability.csv"),
+    htm.absolute_path("gadgeel_1979_permeability.csv"),
     delimiter=",",
 )
 
 steel_1050_permeability_h = Permeability(
     data_T=1000 / (perm_data[:, 0] * u.K**-1),
     data_y=perm_data[:, 1] * u.mol * u.m**-1 * u.s**-1 * u.MPa**-0.5,
-    source="san_marchi_technical_2012",
+    source="gadgeel_gas-phase_1979",
     isotope="H",
-    note="Figure p.1100-23",
+    note="Figure 3",
 )
 
 sol_data = np.genfromtxt(
-    htm.absolute_path("san_marchi_2012_solubility.csv"),
+    htm.absolute_path("gadgeel_1979_solubility.csv"),
     delimiter=",",
 )
 
 steel_1050_solubility_h = Solubility(
     data_T=1000 / (sol_data[:, 0] * u.K**-1),
     data_y=sol_data[:, 1] * u.mol * u.m**-3 * u.MPa**-0.5,
-    source="san_marchi_technical_2012",
+    source="gadgeel_gas-phase_1979",
     isotope="H",
-    note="Figure p.1100-23",
+    note="Figure 5",
 )
 
-properties = [steel_1050_permeability_h, steel_1050_solubility_h]
+diff_data = np.genfromtxt(
+    htm.absolute_path("gadgeel_1979_diffusivity.csv"),
+    delimiter=",",
+)
+
+steel_1050_diffusivity_h = Diffusivity(
+    data_T=1000 / (diff_data[:, 0] * u.K**-1),
+    data_y=diff_data[:, 1] * u.cm**2 * u.s**-1,
+    source="gadgeel_gas-phase_1979",
+    isotope="H",
+    note="Figure 4",
+)
+
+properties = [
+    steel_1050_permeability_h,
+    steel_1050_solubility_h,
+    steel_1050_diffusivity_h,
+]
 
 for prop in properties:
     prop.material = htm.STEEL_1050
